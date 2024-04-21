6e2807ff7cf43155bc0669111c37208e21913f0c
Tom Tarabay Mitchell <z5480769@ad.unsw.edu.au>
2024-04-21 16:38:04 +1000

    Added colours to better distinguish between paths and obstacles

--- basicCA/src/continuousState.py
+++ basicCA/src/continuousState.py
@@ -2,11 +2,17 @@ from sys import argv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.colors as mcolors
 import scipy.signal
 from math import floor, sqrt
 import random as rand
 
-plt.rcParams['image.cmap'] = 'Greys'
+colors = ['grey', 'white', 'blue']
+positions = [0, 0.5, 1]
+custom_cmap = mcolors.LinearSegmentedColormap.from_list("custom_colormap", list(zip(positions, colors)))
+plt.register_cmap(cmap=custom_cmap)
+plt.rcParams['image.cmap'] = 'custom_colormap'
+
 
 def nextIter(state, obstacle, size):
     mask = np.array([[1, 1, 1],
@@ -14,7 +20,7 @@ def nextIter(state, obstacle, size):
                       [1, 1, 1]])
     neighbourSum = scipy.signal.convolve2d(state, mask, mode='same')
     obstacleSum = scipy.signal.convolve2d(obstacle, mask, mode='same')
-    initObstacleSumBoundaries(obstacleSum)
+    updateObstacleSumBoundaries(obstacleSum)
 
     newState = np.empty((size, size))
     for row in range(size):
@@ -34,7 +40,22 @@ def nextIter(state, obstacle, size):
 
     return newState
 
-def initObstacleSumBoundaries(obstacleSum):
+def normaliseState(state):
+    for row in range(size):
+        for col in range(size):
+            state[row, col] = (state[row, col] + 1) / 2
+            if obstacle[row, col] == 1:
+                state[row][col] = 0
+
+def normaliseStateInverse(state):
+    for row in range(size):
+        for col in range(size):
+            state[row, col] = (2 * state[row, col]) - 1
+            if obstacle[row, col] == 1:
+                state[row][col] = 0
+
+
+def updateObstacleSumBoundaries(obstacleSum):
     obstacleSum[0, 0] += 5
     obstacleSum[0, size - 1] += 5
     obstacleSum[1:, 0] += 3
@@ -76,8 +97,12 @@ def initObstacle():
 
 def update(frame, img, state, size):
     img.set_data(state)
+
+    normaliseStateInverse(state)
     newState = nextIter(state, obstacle, size)
+    normaliseState(newState)
     state[:] = newState[:]
+
     return img,
 
 
@@ -87,6 +112,7 @@ fps = float(argv[2])
 obstacle = initObstacle()
 seed = np.zeros((size, size))
 seed[size - 1, :] = 1
+normaliseState(seed)
 
 
 fig, ax = plt.subplots(figsize=(16, 9))
